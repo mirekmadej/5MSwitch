d8bf544c5dc75c96f1bef4c1034d63308d4bba06
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-09-08 20:42:34 +0200

    Add project files.

---
+++ 5MSwitch/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace _5MSwitch
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onSwitchToggled(object sender, EventArgs e)
+        {
+            if (swToggler.IsToggled)
+                lblStan.Text = "stan przełącznika: włączony";
+            else
+                lblStan.Text = "stan przełącznika: wyłączony";
+        }
+
+
+    }
+
+}
